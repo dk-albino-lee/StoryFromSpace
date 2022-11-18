ee84dfa8193472423c2fa32e385c636ba3ae3724
Dong Keun Albino Lee <donglee95.14@gmail.com>
2022-11-19 00:32:30 +0900

    Initial commit

---
+++ app/src/main/java/com/movingroot/storyfromspace/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.movingroot.storyfromspace.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val PrimaryBrown = Color(0xFF56331D)
+val PrimaryOlive = Color(0xFF555839)
+val DarkBrown = Color(0xFF351F12)
+val DarkOlive = Color(0xFF292B1C)
+val LightOlive = Color(0xFF929762)
+val LightBrown = Color(0xFF937A66)
